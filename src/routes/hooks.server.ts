1999c6d02f4365ea274c744012b8dc610a987c46
xayanide <xayanide@gmail.com>
2025-11-29 21:43:22 +0800

    refactor: remove console.log

--- src/routes/hooks.server.ts
+++ src/routes/hooks.server.ts
@@ -2,7 +2,6 @@ import { type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
     const accessToken = event.cookies.get("access_token");
-    console.log(accessToken)
     if (!accessToken) {
         const res = await event.fetch("/api/v1/auth/refresh", { method: "POST" });
         if (!res.ok) {
