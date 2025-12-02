9d4781286f5b5fb34785279bb1a8373b2dfee082
xayanide <xayanide@gmail.com>
2025-12-03 02:37:49 +0800

    refactor: later

--- src/routes/sign-up/+page.server.ts
+++ src/routes/sign-up/+page.server.ts
@@ -9,14 +9,8 @@ export const load = ({ locals }) => {
 
 export const actions: Actions = {
   default: async (event) => {
-    const fd = await event.request.formData();
-    const username = fd.get("username");
-    const email = fd.get("email");
-    const password = fd.get("password");
     const res = await event.fetch("/api/v1/auth/sign-up", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, email, password }),
     });
     if (!res.ok) {
       const data = await res.json();
